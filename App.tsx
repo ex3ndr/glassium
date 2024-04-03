88d6d9771a4f57f44e92cc0516aae4890214f236
Steve Korshakov <steve@korshakov.com>
2024-04-03 12:37:21 -0700

    ref: support multiple protocols, fix mu-law

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { OpusDecoder } from 'opus-decoder';
 import { StatusBar } from 'expo-status-bar';
 import { Button, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 import { openDevice, startBluetooth } from './sources/modules/bt';
@@ -9,6 +8,8 @@ import { delay } from './sources/utils/time';
 import { BTCharacteristic, BTDevice } from './sources/modules/bt_common';
 import { connectAsync, disconnectAsync, startAsync } from './modules/hardware';
 import { WaveFile } from 'wavefile';
+import { ProtocolDefinition, resolveProtocol, startPacketizer } from './sources/modules/protocol';
+import { decode } from './sources/modules/decode';
 
 export default function App() {
   const [logs, log] = useDebugLog();
@@ -37,8 +38,6 @@ export default function App() {
     }
     lastDevice.current = device;
     log('Device opened:' + device.name + ' (' + device.id + ')');
-    let target: BTCharacteristic | null = null;
-    // let targetPSM: BTCharacteristic | null = null;
     for (let service of device.services) {
       log('Service:' + service.id);
       for (let characteristic of service.characteristics) {
@@ -46,93 +45,38 @@ export default function App() {
         log('  - canRead:' + characteristic.canRead);
         log('  - canWrite:' + characteristic.canWrite);
         log('  - canNotify:' + characteristic.canNotify);
-        // if (characteristic.canRead && characteristic.id.toLowerCase() === '19b10003-e8f2-537e-4f6c-d104768a1214') {
-        //   targetPSM = characteristic;
-        // }
-        if (characteristic.canNotify && characteristic.id.toLowerCase() === '19b10001-e8f2-537e-4f6c-d104768a1214') {
-          target = characteristic;
-        }
+      }
+    }
+
+    // Try to find protocol
+    let target: ProtocolDefinition | null = null;
+    let protocol = await resolveProtocol(device);
+    if (protocol) {
+      if (protocol.kind !== 'super' || protocol.codec === 'opus') {
+        log('Unsupported protocol: ' + protocol.codec + ' (' + protocol.kind + ')');
+      } else {
+        target = protocol;
       }
     }
     if (!target) {
-      log('Target characteristic not found');
+      log('Target protocol not found');
       return;
     }
-
-    // Fetch PSM
-    // log('Fetching PSM value...');
-    // let buffer = await targetPSM.read();
-    // if (buffer.length != 2) {
-    //   log('Invalid data');
-    //   return;
-    // }
-    // let dataView = new DataView(buffer.buffer);
-    // let psm = dataView.getUint16(0, true);
-    // log('PSM value: ' + psm);
-
+    log('Target protocol found:' + target.codec);
 
     // Subscribe
     log('Waitign for data...');
     let total = 0;
     let total_packets = 0;
-    let lost = 0
-
-    // GATT
-    let last: { p: number, f: number } | null = null;
-    let frames: Uint8Array[] = [];
-    let pending: Uint8Array = new Uint8Array();
-    let sub = target.subscribe((data) => {
+    let packetizer = startPacketizer();
+    let sub = target.source.subscribe((data) => {
       total += data.length;
       total_packets += 1;
-
-      // Parse packet
-      let value = new Uint8Array(data);
-      let index = (value[0]) + (value[1] << 8);
-      let internal = value[2];
-      let content = value.subarray(3);
-      console.log('Received: ' + index + ' (' + internal + ') - ' + content.length + ' bytes');
-
-      // Wait for first packet
-      if (!last && internal !== 0) {
-        return;
-      }
-
-      if (!last) { // Start of a new frame
-        last = { p: index, f: internal };
-        pending = content;
-      } else {
-        if (index !== last.p + 1) { // Lost frame - reset state
-          console.warn('Lost frame (packet)');
-          last = null;
-          pending = new Uint8Array();
-        } else {
-          if (internal === 0) {// Start of a new frame
-            frames.push(pending);
-            pending = content;
-            last.f = internal;
-            last.p++;
-          } else if (internal === last.f + 1) { // Continue frame
-            pending = new Uint8Array([...pending, ...content]);
-            last.f = internal;
-            last.p++;
-          } else {
-            console.warn('Lost frame (internal)');
-            last = null;
-            pending = new Uint8Array();
-          }
-        }
-      }
-
-      // if (last !== null && (last + 1) !== index) {
-      //   lost += index - (last + 1);
-      // }
-      // last = index
+      packetizer.add(data);
     });
     await delay(10000);
+    let { frames, lost } = packetizer.build();
     sub();
-    if (pending.length > 0) {
-      frames.push(pending);
-    }
 
     // Done
     log('Total received:' + total + ' bytes, speed:' + (total / 10) + ' bytes/sec, lost: ' + lost + ' packets or ' + total_packets + ' packets');
@@ -140,13 +84,11 @@ export default function App() {
 
     // Decode
     log('Decoding...');
-    console.warn(frames);
-    const decoder = new OpusDecoder();
-    await decoder.ready;
-
-    let output = decoder.decodeFrames(frames);
-    const wav = new WaveFile();
-    wav.fromScratch(1, output.sampleRate, '32f', output.channelData[0]);
+    const wav = await decode(target.codec, frames);
+    if (!wav) {
+      log('Failed to decode');
+      return;
+    }
     const base64Wav = wav.toBase64();
     setW(`data:audio/wav;base64,${base64Wav}`);
     log('Done!');

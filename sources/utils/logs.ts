eb922f2115fad3c64dc2bfd99e12e020fa52f168
Steve Korshakov <steve@korshakov.com>
2024-04-21 23:17:54 -0700

    debug: add live logs

--- sources/utils/logs.ts
+++ sources/utils/logs.ts
@@ -1,6 +1,8 @@
+import * as React from 'react';
 import { isDevMode } from "../devmode";
 
 let logs: string[] = [];
+let subscriptions: ((logs: string[]) => void)[] = [];
 
 export function log(tag: string, src: string) {
     if (__DEV__) {
@@ -11,9 +13,18 @@ export function log(tag: string, src: string) {
         if (logs.length > 1000) {
             logs = logs.slice(logs.length - 1000);
         }
+        subscriptions.forEach(s => s(logs));
     }
 }
 
 export function useLogs() {
-    return logs;
+    let [res, set] = React.useState(logs);
+    React.useEffect(() => {
+        let sub = (logs: string[]) => set([...logs]);
+        subscriptions.push(sub);
+        return () => {
+            subscriptions = subscriptions.filter(s => s !== sub);
+        };
+    }, []);
+    return res;
 }

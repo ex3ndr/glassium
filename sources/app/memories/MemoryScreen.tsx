c53c9abaedc13a07e6895edb11ce40c0bf19538c
Steve Korshakov <steve@korshakov.com>
2024-04-28 14:28:05 -0700

    fix: fix memory padding

--- sources/app/memories/MemoryScreen.tsx
+++ sources/app/memories/MemoryScreen.tsx
@@ -4,11 +4,13 @@ import { ScrollView, Text, View } from 'react-native';
 import { Image } from 'expo-image';
 import { Memory } from '../../modules/api/schema';
 import { Theme } from '../../theme';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export const MemoryScreen = React.memo(() => {
+    const safeArea = useSafeAreaInsets();
     let memory = (useRoute().params as any).data as Memory;
     return (
-        <ScrollView style={{ flexGrow: 1, backgroundColor: Theme.background }}>
+        <ScrollView style={{ flexGrow: 1, backgroundColor: Theme.background }} contentContainerStyle={{ paddingBottom: safeArea.bottom }}>
             {memory.image && <Image style={{ height: 256, borderBottomLeftRadius: 16, borderBottomRightRadius: 16 }} source={{ uri: memory.image }} />}
             {!memory.image && <View style={{ height: 16 }} />}
             <View style={{ flexDirection: 'column', flexGrow: 1, flexBasis: 0, paddingTop: 16, paddingHorizontal: 16 }}>

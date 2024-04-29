cff488e921e78d858b85aeb1f44b5f2e0d3ca5f5
Steve Korshakov <steve@korshakov.com>
2024-04-28 20:50:04 -0700

    feat: implement AAC compression in Android

--- modules/audio/android/src/main/java/com/superapp/audio/AudioModule.kt
+++ modules/audio/android/src/main/java/com/superapp/audio/AudioModule.kt
@@ -1,47 +1,138 @@
 package com.superapp.audio
 
+import android.media.MediaCodec
+import android.media.MediaCodecInfo
+import android.media.MediaDataSource
+import android.media.MediaExtractor
+import android.media.MediaFormat
+import android.media.MediaMuxer
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
+import java.io.ByteArrayOutputStream
+import java.io.File
 
 class AudioModule : Module() {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
-  override fun definition() = ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-    // The module will be accessible from `requireNativeModule('Audio')` in JavaScript.
-    Name("Audio")
+    override fun definition() = ModuleDefinition {
+        Name("Audio")
+        AsyncFunction("convert") { value: ByteArray ->
+            return@AsyncFunction compressAudioToAAC(value, appContext.cacheDirectory)
+        }
+    }
+}
 
-    // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
-    Constants(
-      "PI" to Math.PI
-    )
+fun compressAudioToAAC(inputAudio: ByteArray, cacheDir: File): ByteArray {
+    var outputFile: File? = null
+    try {
+        val mediaExtractor = MediaExtractor()
+        mediaExtractor.setDataSource(ByteArrayMediaDataSource(inputAudio))
 
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
+        val audioTrackIndex = selectAudioTrack(mediaExtractor)
+        mediaExtractor.selectTrack(audioTrackIndex)
 
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      "Hello world! 👋"
+        val mediaFormat = mediaExtractor.getTrackFormat(audioTrackIndex)
+        val outputFormat = MediaFormat.createAudioFormat(MediaFormat.MIMETYPE_AUDIO_AAC, mediaFormat.getInteger(MediaFormat.KEY_SAMPLE_RATE), 1)
+        outputFormat.setInteger(MediaFormat.KEY_BIT_RATE, 90000)
+        outputFormat.setInteger(MediaFormat.KEY_AAC_PROFILE, MediaCodecInfo.CodecProfileLevel.AACObjectLC)
+        outputFormat.setInteger(MediaFormat.KEY_MAX_INPUT_SIZE, 65536);
+
+        val mediaCodec = MediaCodec.createEncoderByType(MediaFormat.MIMETYPE_AUDIO_AAC)
+        mediaCodec.configure(outputFormat, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE)
+        mediaCodec.start()
+
+        val buffersInfo = MediaCodec.BufferInfo()
+
+        var outputDone = false
+        var inputDone = false
+
+        outputFile = File.createTempFile("audio-output-", ".m4a", cacheDir);
+        val muxer = MediaMuxer(outputFile.absolutePath, MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4)
+        var audioTrackId = -1
+
+        while (!outputDone) {
+            if (!inputDone) {
+                val inputBufferIndex = mediaCodec.dequeueInputBuffer(10000)
+                if (inputBufferIndex >= 0) {
+                    val buf = mediaCodec.getInputBuffer(inputBufferIndex)!!
+                    val sampleSize = mediaExtractor.readSampleData(buf, 0)
+                    if (sampleSize < 0) {
+                        mediaCodec.queueInputBuffer(inputBufferIndex, 0, 0, 0L, MediaCodec.BUFFER_FLAG_END_OF_STREAM)
+                        inputDone = true
+                    } else {
+                        mediaCodec.queueInputBuffer(inputBufferIndex, 0, sampleSize, mediaExtractor.sampleTime, 0)
+                        mediaExtractor.advance()
+                    }
+                }
+            }
+
+            when (val outputBufferIndex = mediaCodec.dequeueOutputBuffer(buffersInfo, 10000)) {
+                MediaCodec.INFO_OUTPUT_FORMAT_CHANGED -> {
+                    val newFormat = mediaCodec.outputFormat
+                    audioTrackId = muxer.addTrack(newFormat)
+                    muxer.start()
+                }
+
+                MediaCodec.INFO_TRY_AGAIN_LATER -> {
+                    // Ignore
+                }
+
+                else -> {
+                    val outputBuffer = mediaCodec.getOutputBuffer(outputBufferIndex)!!
+                    outputBuffer.position(buffersInfo.offset)
+                    outputBuffer.limit(buffersInfo.offset + buffersInfo.size)
+                    if (audioTrackId >= 0) {
+                        muxer.writeSampleData(audioTrackId, outputBuffer, buffersInfo)
+                    }
+
+                    mediaCodec.releaseOutputBuffer(outputBufferIndex, false)
+                    if (buffersInfo.flags and MediaCodec.BUFFER_FLAG_END_OF_STREAM != 0) {
+                        outputDone = true
+                    }
+                }
+            }
+        }
+        muxer.stop()
+        muxer.release()
+        mediaCodec.stop()
+        mediaCodec.release()
+        mediaExtractor.release()
+
+        return outputFile.readBytes()
+    } finally {
+        outputFile?.delete()
+    }
+}
+
+private fun selectAudioTrack(mediaExtractor: MediaExtractor): Int {
+    for (i in 0 until mediaExtractor.trackCount) {
+        val mediaFormat = mediaExtractor.getTrackFormat(i)
+        val mime = mediaFormat.getString(MediaFormat.KEY_MIME)
+        if (mime?.startsWith("audio/") == true) {
+            return i
+        }
+    }
+    return -1
+}
+
+private class ByteArrayMediaDataSource(private val byteArray: ByteArray) : MediaDataSource() {
+
+    override fun getSize(): Long {
+        return byteArray.size.toLong()
     }
 
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { value: String ->
-      // Send an event to JavaScript.
-      sendEvent("onChange", mapOf(
-        "value" to value
-      ))
+    override fun close() {
+        // Do nothing
     }
 
-    // Enables the module to be used as a native view. Definition components that are accepted as part of
-    // the view definition: Prop, Events.
-    View(AudioView::class) {
-      // Defines a setter for the `name` prop.
-      Prop("name") { view: AudioView, prop: String ->
-        println(prop)
-      }
+    override fun readAt(position: Long, buffer: ByteArray?, offset: Int, size: Int): Int {
+        if (buffer == null) {
+            return -1
+        }
+        if (position >= byteArray.size) {
+            return -1
+        }
+        val remainingBytes = byteArray.size - position.toInt()
+        val bytesToRead = if (size < remainingBytes) size else remainingBytes
+        System.arraycopy(byteArray, position.toInt(), buffer, offset, bytesToRead)
+        return bytesToRead
     }
-  }
 }

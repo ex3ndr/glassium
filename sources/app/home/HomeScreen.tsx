ef9307c467fe934eed22ba016109f7f7e392a515
Steve Korshakov <steve@korshakov.com>
2024-04-30 22:16:27 -0700

    ref: use flash list at home

--- sources/app/home/HomeScreen.tsx
+++ sources/app/home/HomeScreen.tsx
@@ -10,6 +10,7 @@ import { backoff } from '../../utils/time';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { useRouter } from '../../routing';
 import { HomeNotification } from './HomeNotification';
+import { FlashList } from '@shopify/flash-list';
 
 function useMemories() {
     const app = useAppModel();
@@ -41,15 +42,37 @@ function useMemories() {
 export const HomeScreen = React.memo(() => {
     const router = useRouter();
     const safeArea = useSafeAreaInsets();
-    const app = useAppModel();
-    const realtime = app.realtime.use();
-    const captureState = useAtomValue(app.capture.captureState);
     const memories = useMemories();
+
+    // Render list
+    if (memories !== null && memories.length > 0) {
+        return (
+            <View style={{ flexGrow: 1 }}>
+                <FlashList
+                    data={memories}
+                    keyExtractor={(item) => item.id}
+                    ListHeaderComponent={<HomeNotification />}
+                    renderItem={({ item }) => (
+                        <Pressable key={item.id} style={{ borderRadius: 32, marginHorizontal: 16, marginVertical: 16, flexDirection: 'row' }} onPress={() => router.navigate('memory', { data: item })}>
+                            {item.image ? (
+                                <Image source={{ uri: item.image }} style={{ width: 64, height: 64, borderRadius: 32, borderWidth: 0.5, borderColor: '#e9e9e9' }} />
+                            ) : <View style={{ width: 64, height: 64, borderRadius: 32, backgroundColor: Theme.textSecondary }} />}
+                            <View style={{ flexDirection: 'column', flexGrow: 1, flexBasis: 0, marginLeft: 16 }}>
+                                <Text style={{ fontSize: 16, color: Theme.text }} numberOfLines={3}>{item.title}</Text>
+                                <Text style={{ fontSize: 14, opacity: 0.7, color: Theme.text }} numberOfLines={1}>{item.summary.replaceAll('\n', ' ')}</Text>
+                            </View>
+                        </Pressable>
+                    )}
+                />
+            </View>
+
+        )
+    }
+
     return (
         <View style={{ flexGrow: 1 }}>
             <ScrollView style={{}} contentContainerStyle={{ paddingBottom: 64 + safeArea.bottom, flexGrow: 1 }} alwaysBounceVertical={false} scrollIndicatorInsets={{ top: 0, bottom: 64 }}>
                 <HomeNotification />
-
                 {/* {captureState.streaming && (
                     <>
                         <View style={{ marginHorizontal: 16, paddingHorizontal: 16, backgroundColor: '#445ef1', borderRadius: 32, paddingVertical: 16, marginVertical: 16, height: 128 }}>
@@ -69,20 +92,7 @@ export const HomeScreen = React.memo(() => {
                         </Text>
                     </View>
                 )}
-                {memories !== null && memories.length > 0 && (
-                    memories.map((memory) => (
-                        <Pressable key={memory.id} style={{ borderRadius: 32, marginHorizontal: 16, marginVertical: 16, flexDirection: 'row' }} onPress={() => router.navigate('memory', { data: memory })}>
-                            {memory.image ? (
-                                <Image source={{ uri: memory.image }} style={{ width: 64, height: 64, borderRadius: 32, borderWidth: 0.5, borderColor: '#e9e9e9' }} />
-                            ) : <View style={{ width: 64, height: 64, borderRadius: 32, backgroundColor: Theme.textSecondary }} />}
-                            <View style={{ flexDirection: 'column', flexGrow: 1, flexBasis: 0, marginLeft: 16 }}>
-                                <Text style={{ fontSize: 16, color: Theme.text }} numberOfLines={3}>{memory.title}</Text>
-                                <Text style={{ fontSize: 14, opacity: 0.7, color: Theme.text }} numberOfLines={1}>{memory.summary.replaceAll('\n', ' ')}</Text>
-                            </View>
-                        </Pressable>
-                    ))
-                )}
             </ScrollView>
-        </View >
+        </View>
     );
 });

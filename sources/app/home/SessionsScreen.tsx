e315cdbd1f0e54e3a7df7eced5bef479ad4cdc7a
Steve Korshakov <steve@korshakov.com>
2024-05-03 00:34:20 -0700

    feat: show classification results

--- sources/app/home/SessionsScreen.tsx
+++ sources/app/home/SessionsScreen.tsx
@@ -1,15 +1,48 @@
 import * as React from 'react';
-import { ActivityIndicator, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, Pressable, Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Theme } from '../../theme';
 import { useAppModel } from '../../global';
 import { useRouter } from '../../routing';
+import { ViewSession } from '../../modules/state/SessionsModel';
+import { FlashList } from '@shopify/flash-list';
+
+const SessionComponent = React.memo((props: { session: ViewSession }) => {
+    const router = useRouter();
+    let status: string = props.session.state;
+    if (props.session.classification === 'boring') {
+        status = 'boring 🥱';
+    } else if (props.session.classification === 'important') {
+        status = 'important 🤩';
+    } else if (props.session.classification === 'broken') {
+        status = 'broken 🚧';
+    } else if (props.session.classification === 'empty') {
+        status = 'empty 🤔';
+    }
+    return (
+        <Pressable
+            style={(p) => ({
+                backgroundColor: p.pressed ? '#111111' : '#1c1c1c',
+                marginHorizontal: 16,
+                marginVertical: 8,
+                borderRadius: 16,
+                paddingHorizontal: 16,
+                paddingVertical: 18,
+                flexDirection: 'row'
+            })}
+            onPress={() => { router.navigate('session', { id: props.session.id }) }}
+        >
+            <Text style={{ color: Theme.text, fontSize: 24, flexGrow: 1, flexBasis: 0, alignSelf: 'center' }}>Session #{(props.session.index + 1)}</Text>
+            <Text style={{ color: Theme.text, alignSelf: 'center' }}>{status}</Text>
+        </Pressable>
+    );
+});
 
 export const SessionsScreens = React.memo(() => {
     const safeArea = useSafeAreaInsets();
     const appModel = useAppModel();
     const sessions = appModel.useSessions();
-    const router = useRouter();
+
     return (
         <View style={{ flexGrow: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: Theme.background }}>
             {sessions === null && (
@@ -18,31 +51,20 @@ export const SessionsScreens = React.memo(() => {
                 </View>
             )}
             {sessions !== null && sessions.length === 0 && (
-                <View style={{ flexGrow: 1, paddingBottom: safeArea.bottom, }}>
-                    <Text>Press record button to start!</Text>
+                <View style={{ flexGrow: 1, paddingBottom: safeArea.bottom }}>
+                    <Text>No sessions</Text>
                 </View>
             )}
             {sessions !== null && sessions.length > 0 && (
-                <ScrollView style={{ flexGrow: 1, flexBasis: 0, alignSelf: 'stretch' }} contentContainerStyle={{ alignItems: 'stretch', paddingBottom: safeArea.bottom + 64 }}>
-                    {sessions.map((session) => (
-                        <Pressable
-                            key={session.id}
-                            style={(p) => ({
-                                backgroundColor: p.pressed ? '#111111' : '#1c1c1c',
-                                marginHorizontal: 16,
-                                marginVertical: 8,
-                                borderRadius: 16,
-                                paddingHorizontal: 16,
-                                paddingVertical: 18,
-                                flexDirection: 'row'
-                            })}
-                            onPress={() => { router.navigate('session', { id: session.id }) }}
-                        >
-                            <Text style={{ color: Theme.text, fontSize: 24, flexGrow: 1, flexBasis: 0, alignSelf: 'center' }}>Session #{(session.index + 1)}</Text>
-                            <Text style={{ color: Theme.text, alignSelf: 'center' }}>Status: {session.state}</Text>
-                        </Pressable>
-                    ))}
-                </ScrollView>
+                <View style={{ flexGrow: 1, alignSelf: 'stretch' }}>
+                    <FlashList
+                        contentContainerStyle={{ paddingBottom: safeArea.bottom + 64 }}
+                        data={sessions}
+                        renderItem={({ item }) => <SessionComponent key={item.id} session={item} />}
+                        estimatedItemSize={80}
+                        keyExtractor={(item) => item.id}
+                    />
+                </View>
             )}
         </View>
     );

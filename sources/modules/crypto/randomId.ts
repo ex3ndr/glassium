31a9c76ea59cf0ae77596a7fdefa80102530cba7
Steve Korshakov <steve@korshakov.com>
2024-05-04 15:49:58 -0700

    fix: fix randomId typo

--- sources/modules/crypto/randomId.ts
+++ sources/modules/crypto/randomId.ts
@@ -1,6 +1,6 @@
 import * as Crypto from 'expo-crypto';
 
 export function randomId(length = 16) {
-    let data = Crypto.getRandomBytes(16);
-    return data.reduce((acc, byte) => acc + byte.toString(length).padStart(2, '0'), '');
+    let data = Crypto.getRandomBytes(length);
+    return data.reduce((acc, byte) => acc + byte.toString(16).padStart(2, '0'), '');
 }

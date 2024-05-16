3dc62d4de9c1ac9467727c86c3a6b8e0d43f120f
Steve Korshakov <steve@korshakov.com>
2024-05-15 18:29:52 -0700

    ref: social.png -> social.jpg

--- sources/app/routes/_head.tsx
+++ sources/app/routes/_head.tsx
@@ -8,7 +8,7 @@ export const DefaultMetatags = () => {
             <meta property="og:title" content="Glassium" />
             <meta property="og:site_name" content="Glassium" />
             <meta property="og:description" content="All-in-one app for AI Wearable devices" />
-            <meta property="og:image" content="/social.png" />
+            <meta property="og:image" content="/social.jpg" />
             <meta property="og:image:width" content="1200" />
             <meta property="og:image:height" content="630" />
             <meta property="og:type" content="website"/>

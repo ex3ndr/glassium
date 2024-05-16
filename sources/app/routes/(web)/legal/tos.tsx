2b8759ff3e5d635a0a985e7598fc9b3d32e7b4fa
Steve Korshakov <steve@korshakov.com>
2024-05-15 21:06:32 -0700

    feat: redirect to tos/privacy policy

--- sources/app/routes/(web)/legal/tos.tsx
+++ sources/app/routes/(web)/legal/tos.tsx
@@ -1,20 +1,4 @@
-import { View } from "react-native";
-
+import { Redirect } from "expo-router";
 export default function Privacy() {
-    return (
-        <View style={{ flexGrow: 1 }}>
-            <div
-                dangerouslySetInnerHTML={{
-                    __html: `
-                <div name="termly-embed" data-id="a77dbfd8-20d7-42ca-9e3c-7779f1c24459"></div>
-                <script type="text/javascript">(function(d, s, id) {
-      var js, tjs = d.getElementsByTagName(s)[0];
-                    if (d.getElementById(id)) return;
-                    js = d.createElement(s); js.id = id;
-                    js.src = "https://app.termly.io/embed-policy.min.js";
-                    tjs.parentNode.insertBefore(js, tjs);
-    }(document, 'script', 'termly-jssdk'));</script>
-            `}} />
-        </View>
-    )
+    return <Redirect href={"https://app.termly.io/policy-viewer/policy.html?policyUUID=5e067a7a-e3cf-4fae-9740-4b9d30f9facf"} />
 }
